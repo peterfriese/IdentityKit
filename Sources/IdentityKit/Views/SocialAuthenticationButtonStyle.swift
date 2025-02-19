4b63a16e43a2b6e57aa33d8251644f32aa57788d
Peter Friese <peter@peterfriese.de>
2025-02-19 10:53:09 +0100

    Update copyright
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- Sources/IdentityKit/Views/SocialAuthenticationButtonStyle.swift
+++ Sources/IdentityKit/Views/SocialAuthenticationButtonStyle.swift
@@ -2,7 +2,7 @@
 // SocialAuthenticationButtonStyle.swift
 // IdentityKit
 //
-// Created by Alex on 18.02.25.
+// Created by Peter Friese on 18.02.25.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
